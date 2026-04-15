811f05d96878d6c012ad8a9cff9313a8fdb71b46
Humza Butt <Humzab1711@hotmail.com>
2026-04-15 14:02:03 +0100

    Add Privacy page and BannerGate component to App layout, update main container styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,18 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
 import Contact from './pages/Contact'
+import Privacy from './pages/Privacy'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import CrisisFooter from './components/layout/CrisisFooter'
+import BannerGate from './components/layout/BannerGate'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col bg-cream">
+        <BannerGate />
         <Navbar />
         <main className="flex-grow">
           <Routes>
@@ -19,6 +22,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy" element={<Privacy />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
